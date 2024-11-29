c76c7097645ab66f549a774c086456aea91532f4
wirtzpendragon <adityawiranta2018@gmail.com>
2024-11-29 21:52:49 +0800

    text size

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
     <div className="mt-40 lg:mt-64 text-appGreen-900 font-inter lg:w-10/12 mx-auto mb-40 lg:mb-64">
       <h1 className="text-center font-bold  text-4xl">ABOUT ME</h1>
     <div>
-      <p className="text-center lg:text-lg mx-4 mt-4 lg:mx-56">My name is Made Suastika Aditya Wiranata, I'm 17 years old. I am a student of SMK Wira Harapan (Tegal Jaya), a person who can fast learner, honest and discipline. I enjoy to learn new things and get a new experience. I can work as a team and personal I love coding and playing games.</p>
+      <p className="text-center text-xs lg:text-lg mx-4 mt-4 lg:mx-56">My name is Made Suastika Aditya Wiranata, I'm 17 years old. I am a student of SMK Wira Harapan (Tegal Jaya), a person who can fast learner, honest and discipline. I enjoy to learn new things and get a new experience. I can work as a team and personal I love coding and playing games.</p>
     </div>
 
     <div>
@@ -72,7 +72,7 @@ export default function Home() {
     className="w-full mx-auto my-5"
     />
         <h2 className="text-center font-semibold text-2xl ">S&P BEVERAGES</h2>
-        <p className="m-2"> A user-friendly product catalog showcasing a variety of beverages with vibrant visuals. Each product displays a name, price, rating, and a "Buy" button for quick purchasing, providing an engaging and accessible online shopping experience.</p>
+        <p className="m-2 text-xs lg:text-base"> A user-friendly product catalog showcasing a variety of beverages with vibrant visuals. Each product displays a name, price, rating, and a "Buy" button for quick purchasing, providing an engaging and accessible online shopping experience.</p>
         <h3 className="m-2 font-semibold text-lg underline">Bootstrap</h3>
     </div>
 
@@ -83,7 +83,7 @@ export default function Home() {
     className="w-full mx-auto my-5"
     />
         <h2 className="text-center font-semibold text-2xl ">ADMIN LTE</h2>
-        <p className="m-2"> A modern and responsive dashboard designed for data management. It features key statistics such as the number of students, teachers, classes, and majors. The interface includes a detailed table for viewing student information with options for actions, making it efficient for administrators to manage school data.</p>
+        <p className="m-2 text-xs lg:text-base"> A modern and responsive dashboard designed for data management. It features key statistics such as the number of students, teachers, classes, and majors. The interface includes a detailed table for viewing student information with options for actions, making it efficient for administrators to manage school data.</p>
         <span className="flex mt-2">
         <h3 className="mr-2 font-semibold text-lg underline">PHP</h3>
         <h3 className="mr-2 font-semibold text-lg underline">Admin LTE</h3>
@@ -95,7 +95,7 @@ export default function Home() {
     className="w-full mx-auto my-5"
     />
         <h2 className="text-center font-semibold text-2xl ">CHESSBOARD</h2>
-        <p className="m-2"> A visually appealing chessboard application created in Java, featuring a complete and accurately arranged starting position for chess pieces. It serves as a graphical interface for playing or developing chess games, showcasing Java's capability for creating interactive and dynamic visuals.</p>
+        <p className="m-2 text-xs lg:text-base"> A visually appealing chessboard application created in Java, featuring a complete and accurately arranged starting position for chess pieces. It serves as a graphical interface for playing or developing chess games, showcasing Java's capability for creating interactive and dynamic visuals.</p>
         <h3 className="m-2 font-semibold text-lg underline">JAVA</h3>
     </div>
 

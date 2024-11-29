15ccdb8fb5fe44bcfe5a93e9b3bcdc69d813733f
wirtzpendragon <adityawiranta2018@gmail.com>
2024-11-29 21:54:03 +0800

    border profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
     <div className="w-6/12">
     <Image 
     src={ImageProfile}
-    className="border  w-full rounded-full"
+    className="border-4  w-full rounded-full border-appGreen-500"
     />
     </div>
     </div>

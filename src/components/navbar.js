fa6301d51e2d105c5497d799b1ef0fad7706439c
wirtzpendragon <adityawiranta2018@gmail.com>
2024-11-29 21:42:54 +0800

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ export default function Navbar({ children }) {
     return (
       <>
       
-      <div className="flex flex-row w-screen h-14 items-center justify-around font-inter font-medium text-lg bg-appGreen-900 text-primary px-4">
+      <div className="flex flex-row w-screen h-14 items-center justify-around font-inter lg:font-medium text-base lg:text-lg bg-appGreen-900 text-primary px-4">
         <p>Aditya Wiranata</p>
         <span className="flex ">
         <a href="#" className="hover:scale-125 m-5"> About</a>

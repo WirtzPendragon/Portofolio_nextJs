6555cbb0c087a0e47da35718685d51a530802b31
wirtzpendragon <adityawiranta2018@gmail.com>
2024-11-29 21:48:22 +0800

    navbar margin

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,12 +3,12 @@ export default function Navbar({ children }) {
     return (
       <>
       
-      <div className="flex flex-row w-screen h-14 items-center justify-around font-inter lg:font-medium text-xs lg:text-lg bg-appGreen-900 text-primary px-4">
+      <div className="flex flex-row w-screen h-14 items-center justify-around font-inter font-medium text-xs lg:text-lg bg-appGreen-900 text-primary px-4">
         <p>Aditya Wiranata</p>
         <span className="flex ">
-        <a href="#" className="hover:scale-125 m-5"> About</a>
-        <a href="#" className="hover:scale-125 m-5"> Project</a>
-        <a href="#" className="hover:scale-125 m-5"> Contact</a>
+        <a href="#" className="hover:scale-125 my-5 mr-2"> About</a>
+        <a href="#" className="hover:scale-125 my-5 mr-2"> Project</a>
+        <a href="#" className="hover:scale-125 my-5 mr-2"> Contact</a>
         </span>        
       </div>
       <div>{ children }</div>
